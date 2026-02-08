a3235605c69582a762c68a192fa994e19c88a146
andres2389 <aframirez@soy.sena.edu.co>
2026-02-08 16:05:44 -0500

    actualizacion

--- models/Product.js
+++ models/Product.js
@@ -7,7 +7,6 @@ const productSchema = new mongoose.Schema(
     valor: { type: Number, required: true },
     descripcion: { type: String, required: true },
     imagen: { type: String },
-    talla: { type: [String], default: [] }, // Array de tallas disponibles
 
   },
   { timestamps: true }
